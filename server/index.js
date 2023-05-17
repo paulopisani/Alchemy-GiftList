88957797559d89809979b694486a419e1ba8a217
PauloJP <paulopisani@gmail.com>
2023-05-17 14:49:11 -0300

    Change to check if name provided by Client is in Mekle Tree

--- server/index.js
+++ server/index.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const verifyProof = require('../utils/verifyProof');
+const niceList = require('../utils/niceList');
+const MerkleTree = require('../utils/MerkleTree');
 
 const port = 1225;
 
@@ -8,19 +10,27 @@ app.use(express.json());
 
 // TODO: hardcode a merkle root here representing the whole nice list
 // paste the hex string in here, without the 0x prefix
-const MERKLE_ROOT = '';
+const MERKLE_ROOT = "03d9bbe34b1cd570674a4524ba3e409005590f96b0b5615d13bf97282f7d8399";
+//const myMerkle = new MerkleTree(niceList);
+//console.log('MERKLE_ROOT:::::::::::',MERKLE_ROOT);
+
 
 app.post('/gift', (req, res) => {
   // grab the parameters from the front-end here
   const body = req.body;
+  const name = req.body.name;
+  const proof = req.body.proof;
+
+  //console.log('Teste nome;', name);
 
   // TODO: prove that a name is in the list 
-  const isInTheList = false;
+
+  const isInTheList = verifyProof(proof, name, MERKLE_ROOT);
   if(isInTheList) {
-    res.send("You got a toy robot!");
+    res.send("###################You got a toy Tree!");
   }
   else {
-    res.send("You are not on the list :(");
+    res.send("!!!!!!!!!!!!!!!!!!You are not on the list :( ");
   }
 });
 
