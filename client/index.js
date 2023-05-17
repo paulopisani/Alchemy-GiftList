ce5f9afe57012e84c6df897e5e58ddb5478cd49a
PauloJP <paulopisani@gmail.com>
2023-05-17 14:51:18 -0300

    Changes to check if name is in Merkle Tree

--- client/index.js
+++ client/index.js
@@ -6,9 +6,17 @@ const serverUrl = 'http://localhost:1225';
 
 async function main() {
   // TODO: how do we prove to the server we're on the nice list? 
+  const myMerkle = new MerkleTree(niceList);
+  
+  const name = 'Miss Michael Prosacco III';
+  const index = niceList.findIndex(n => n === name);
+  const proof = myMerkle.getProof(index);
+  
 
   const { data: gift } = await axios.post(`${serverUrl}/gift`, {
     // TODO: add request body parameters here!
+    name : name,
+    proof: proof
   });
 
   console.log({ gift });
